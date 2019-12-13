a85c57977ba158efbedd4fe0b669691fdff3ab76
Alexey Hotchenkov <hotchenkov@list.ru>
2019-12-13 23:17:31 +0300

    Initial Commit

---
+++ SausageMakerHelper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SausageMakerHelper
+//
+//  Created by Алексей Хотченков on 13/12/2019.
+//  Copyright © 2019 ru.cadovnikxyan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
