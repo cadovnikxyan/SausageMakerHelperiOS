8986a554f16e028ee4c317ae5cb4711d0d32b339
Alexey Hotchenkov <hotchenkov@list.ru>
2020-01-15 01:01:21 +0300

    Created simple main view

--- SausageMakerHelper/ContentView.swift
+++ SausageMakerHelper/ContentView.swift
@@ -9,10 +9,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showDetails = false
     var body: some View {
-        Text("Hello, World!")
+        TabView {
+                  Text("Main")
+                      .tabItem {
+                        Image(systemName: "list.dash")
+                          Text("Main")
+                      }.tag(0)
+                  Text("Sausage Calculator")
+                      .tabItem {
+                          Image(systemName: "plus.slash.minus")
+                          Text("Calculator")
+                      }.tag(1)
+                  Text("Archive")
+                      .tabItem {
+                          Image(systemName: "tray")
+                          Text("Archive")
+                      }.tag(2)
+              }
+        }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
